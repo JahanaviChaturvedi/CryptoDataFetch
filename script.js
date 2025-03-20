d4a6d45939a06f3147c19f50cb2df84eb146a5bf
JahanaviChaturvedi <118982130+JahanaviChaturvedi@users.noreply.github.com>
2025-03-20 13:06:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+let cryptoData = [];
+
+function fetchData() {
+  fetch(
+    "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false"
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      cryptoData = data;
+      dataRender(data);
+    })
+    .catch((err) => console.erorr("Error fetching data", err));
+}
+
+function dataRender(data) {
+  const tbody = document.querySelector(".cryptoTable tbody");
+  tbody.innerHML = "";
+  data.forEach((coin) => {
+    const row = `<tr>
+    <td><img src="${coin.image}" alt="${coin.name}" width="25"</td>
+    <td>${coin.name}<td>
+    <td>${coin.symbol.toUpperCase()}</td>
+    <td>${coin.current_price}<td>
+    <td>${coin.total_volume}<td>
+    <td>${coin.prive_change__percentage_24th}<td>
+    </tr>`;
+    tbody.innerHTML += row;
+  });
+}
+
+fetchData();
